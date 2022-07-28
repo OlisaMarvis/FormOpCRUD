607487d2ddd832edb3a0e7405f84d6283859aae5
OlisaMarvis <Olisamarvis@gmail.com>
2022-07-28 13:17:21 +0100

    Add project files.

---
+++ FormOpCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FormOpCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormOpCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FormOpCRUD/Migrations/20220728061534_Initialise.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormOpCRUD.Migrations
+{
+    public partial class Initialise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Employees",
+                newName: "DateOfBirth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Employees",
+                newName: "DateTime");
+        }
+    }
+}
